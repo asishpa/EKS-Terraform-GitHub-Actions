5428fc64ada65f2015332fce397c845deaaefd3a
Asish Patro <apatro817@gmail.com>
2024-08-24 10:47:23 +0530

    added dynamodb table name

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "devsecops-bucketone"
     region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "devsecops-dynamodb-table"
     encrypt        = true
   }
 }
